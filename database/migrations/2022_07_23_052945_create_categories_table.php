f05940586e8efed780b9b4dacea06fed9a98c5d4
sanjaykumar <sanjay.nishad@fluper.in>
2022-07-23 13:04:05 +0530

    changes done

--- database/migrations/2022_07_23_052945_create_categories_table.php
+++ database/migrations/2022_07_23_052945_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
         $table->bigIncrements('id');
         $table->string('name');
         $table->unsignedBigInteger('category_id')->nullable();
-        $table->foreign('category_id')->references('id')->on('categories');
+        $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         $table->timestamps();
         });
     }
